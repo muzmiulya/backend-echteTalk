265b72519a612eeccab675dc4c8eab093edaf8cd
muzmiulya <muhammadmuzmiulya@gmail.com>
2020-10-22 09:39:10 +0700

    check error

--- src/controller/profile.js
+++ src/controller/profile.js
@@ -77,7 +77,8 @@ module.exports = {
                 return helper.response(response, 404, `Profile By Id: ${id} Not Found`)
             }
         } catch (error) {
-            return helper.response(response, 400, "Bad Request", error)
+            // return helper.response(response, 400, "Bad Request", error)
+            console.log(error)
         }
     },
 }
